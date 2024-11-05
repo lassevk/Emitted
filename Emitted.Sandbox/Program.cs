45b10c2e4e373f05cab1f891b8dd89bf2f9a0a74
Lasse Vågsæther Karlsen <lasse@vkarlsen.no>
2024-11-05 19:35:34 +0100

    More methods

--- Emitted.Sandbox/Program.cs
+++ Emitted.Sandbox/Program.cs
@@ -1,3 +1,36 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using System.Reflection.Emit;
 
-Console.WriteLine("Hello, World!");
+using Emitted;
+
+var method = new DynamicMethod("method", typeof(void), [typeof(string)]);
+ILGenerator il = method.GetILGenerator();
+
+Label lTooShort = il.DefineLabel();
+Label lExit = il.DefineLabel();
+il
+   .local<string>()
+   .ldstr("Hello, {0}!")
+   .ldarg_0()
+   .dup()
+   .stloc_0()
+   .call(typeof(string).GetMethod("Format", [typeof(string), typeof(string)])!)
+   .call(typeof(Console).GetMethod("WriteLine", [typeof(string)])!)
+   .ldstr("i = {0}")
+   .ldloc_0()
+   .ldlen()
+   .dup()
+   .ldc_i4_4()
+   .ble(lTooShort)
+   .box<int>()
+   .call(typeof(string).GetMethod("Format", [typeof(string), typeof(object)])!)
+   .call(typeof(Console).GetMethod("WriteLine", [typeof(string)])!)
+   .br(lExit)
+   .mark(lTooShort)
+   .pop()
+   .pop()
+   .mark(lExit)
+   .ret();
+
+Action<string> mm = method.CreateDelegate<Action<string>>();
+mm("World");
+mm("Worl");
--- src/Emitted/ILGeneratorExtensions.cs
+++ src/Emitted/ILGeneratorExtensions.cs
@@ -1,8 +1,123 @@
+using System;
+using System.Reflection;
+using System.Reflection.Emit;
+using System.Runtime.InteropServices;
+
 using JetBrains.Annotations;
+// ReSharper disable InconsistentNaming
 
 namespace Emitted;
 
 [PublicAPI]
 public static partial class ILGeneratorExtensions
 {
+    public static LocalBuilder DeclareLocal<T>(this ILGenerator il, bool pinned = false)
+    {
+        Guard.NotNull(il);
+        return il.DeclareLocal(typeof(T), pinned);
+    }
+
+    public static ILGenerator local(this ILGenerator il, Type type, bool pinned = false)
+    {
+        Guard.NotNull(il);
+        il.DeclareLocal(type, pinned);
+        return il;
+    }
+
+    public static ILGenerator local<T>(this ILGenerator il, bool pinned = false) => local(il, typeof(T), pinned);
+
+    public static ILGenerator mark(this ILGenerator il, Label label)
+    {
+        Guard.NotNull(il);
+        Guard.NotNull(label);
+        il.MarkLabel(label);
+        return il;
+    }
+
+    public static ILGenerator call(this ILGenerator il, MethodInfo method)
+    {
+        Guard.NotNull(il);
+        Guard.NotNull(method);
+        il.Emit(OpCodes.Call, method);
+        return il;
+    }
+
+    public static ILGenerator call(this ILGenerator il, ConstructorInfo constructor)
+    {
+        Guard.NotNull(il);
+        Guard.NotNull(constructor);
+        il.Emit(OpCodes.Call, constructor);
+        return il;
+    }
+
+    public static ILGenerator call(this ILGenerator il, MethodInfo method, Type[] optionalParameterTypes)
+    {
+        Guard.NotNull(il);
+        Guard.NotNull(method);
+        il.EmitCall(OpCodes.Call, method, optionalParameterTypes);
+        return il;
+    }
+
+    public static ILGenerator calli(this ILGenerator il, CallingConventions callingConvention, Type? returnType, Type[]? parameterTypes, Type[]? optionalParameterTypes)
+    {
+        Guard.NotNull(il);
+        il.EmitCalli(OpCodes.Calli, callingConvention, returnType, parameterTypes, optionalParameterTypes);
+        return il;
+    }
+
+    public static ILGenerator calli(this ILGenerator il, CallingConvention callingConvention, Type? returnType, Type[]? parameterTypes)
+    {
+        Guard.NotNull(il);
+        il.EmitCalli(OpCodes.Calli, callingConvention, returnType, parameterTypes);
+        return il;
+    }
+
+    public static ILGenerator callvirt(this ILGenerator il, MethodInfo method)
+    {
+        Guard.NotNull(il);
+        Guard.NotNull(method);
+        il.Emit(OpCodes.Callvirt, method);
+        return il;
+    }
+
+    public static ILGenerator callvirt(this ILGenerator il, MethodInfo method, Type[] optionalParameterTypes)
+    {
+        Guard.NotNull(il);
+        Guard.NotNull(method);
+        il.EmitCall(OpCodes.Callvirt, method, optionalParameterTypes);
+        return il;
+    }
+
+    /// <summary>
+    /// Emits one of the "Ldc_I4_*" opcodes, depending on the value.
+    /// </summary>
+    /// <param name="il">
+    /// The <see cref="ILGenerator"/> to emit the opcode to.
+    /// </param>
+    /// <param name="value">
+    /// The value to push onto the stack.
+    /// </param>
+    /// <returns>
+    /// The <see cref="ILGenerator"/> passed in through <paramref name="il"/>.
+    /// </returns>
+    public static ILGenerator ldc_auto(this ILGenerator il, int value)
+    {
+        return value switch
+        {
+            -1                 => il.ldc_i4_m1(),
+            0                  => il.ldc_i4_0(),
+            1                  => il.ldc_i4_1(),
+            2                  => il.ldc_i4_2(),
+            3                  => il.ldc_i4_3(),
+            4                  => il.ldc_i4_4(),
+            5                  => il.ldc_i4_5(),
+            6                  => il.ldc_i4_6(),
+            7                  => il.ldc_i4_7(),
+            8                  => il.ldc_i4_8(),
+            >= -128 and <= 127 => il.ldc_i4_s((sbyte)value),
+            _                  => il.ldc_i4(value),
+        };
+    }
+
+    public static ILGenerator box<T>(this ILGenerator il) => il.box(typeof(T));
 }
